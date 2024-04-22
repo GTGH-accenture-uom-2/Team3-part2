dde526026637861e34d48d5935dbfc4fe5bcfca4
vasoKarouta <vasokarouta@gmail.com>
2024-04-22 17:04:20 +0300

    reservation

--- src/main/java/gr/accenture/team3/services/ReservationService.java
+++ src/main/java/gr/accenture/team3/services/ReservationService.java
@@ -53,23 +53,53 @@ public class ReservationService {
 
 
     public Reservation addNewReservation(String amka, Long id, String surname){
-        for(Insured insured : insuredService.insureds){
-            if(insured.getAmka().equals(amka)){
-                for(Timeslot timeslot: timeslotService.timeslots){
-                    if(timeslot.getId().equals(id)){
-                        for(Doctor doctor : doctorService.doctors){
-                            if (doctor.getSurname().equals(surname)){
-                                timeslot.setDoctor(doctor);
-                                Reservation reservation = new Reservation(insured,timeslot,timeslot.getDate());
-                                reservations.add(reservation);
-                                return reservation;
-                            }
-                        }
-                    }
-                }
+        Insured insured = insuredService.getInsuredByAmka(amka);
+        for(Reservation reservation: reservations){
+            if(reservation.getInsured().equals(insured)){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You can up one reservation!");
             }
         }
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Incorrect data!");
+        Timeslot timeslot = timeslotService.getTimeslotById(id);
+        Doctor doctor = doctorService.getDoctorBySurname(surname);
+        if(timeslot.getDoctor()==null){
+            timeslot.setDoctor(doctor);
+            Reservation reservation = new Reservation(insured,timeslot,timeslot.getDate());
+            reservations.add(reservation);
+            return reservation;
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The requested timeslot is not free.");
+    }
+
+    public Reservation changeReservation(String amka, Long id){
+        //search in the list of reservations for the old reservation
+        Reservation reservation = reservations.stream()
+                .filter(r -> r.getInsured().getAmka().equals(amka))
+                .findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "There is no booking with this amka!"));
+
+        //check whether the insured has made up to two changes
+        Insured insured = reservation.getInsured();
+        insured.increaseChangeReservationCounter();
+        if(insured.getChangeReservationsCounter()>2)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You can make up to two changes.");
+
+        //find the old timeslot and the new timeslot
+        Timeslot oldReservationTimeslot = reservation.getTimeslot();
+        Timeslot newTimeslot = timeslotService.getTimeslotById(id);
+        //check if the new timeslot is free
+        if(newTimeslot.getDoctor() != null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The requested timeslot is not free.");
+
+        //update the doctor to the new timeslot
+        newTimeslot.setDoctor(oldReservationTimeslot.getDoctor());
+        oldReservationTimeslot.setDoctor(null);
+
+        //create a new reservation and delete the old one from the list reservations
+        Reservation changedReservation = new Reservation(insured,newTimeslot,newTimeslot.getDate());
+        reservations.add(changedReservation);
+        reservations.remove(reservation);
+        return changedReservation;
+
     }
 
     public Reservation getAReservation(String amka){
